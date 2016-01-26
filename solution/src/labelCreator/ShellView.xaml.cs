bd3bae5ab47d14a32e4bf088827de1995b5953df
laurianesavary <lauriane.savary@gmail.com>
2016-01-26 20:42:52 +0100

    First empty MahApps window

---
+++ solution/src/labelCreator/ShellView.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace labelCreator
+{
+    using MahApps.Metro.Controls;
+
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class ShellView : MetroWindow
+    {
+        public ShellView()
+        {
+            InitializeComponent();
+        }
+    }
+}
