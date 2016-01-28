9d2b4ca016fbd905dee2b072b70939bd23f3f4bb
laurianesavary <lauriane.savary@gmail.com>
2016-01-28 15:18:48 +0100

    Management of the flyout
    - Use Caliburn, StyleCop
    - Flyout is implemented (auto-close on focus lost)
    - Namespace has been renamed to fix StyleCop warnings

---
+++ solution/src/labelCreator/ViewModels/Contracts/ISettingsFlyoutViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace LabelCreator.ViewModels.Contracts
+{
+    /// <summary>
+    ///     Defines the contract for the view model representing the application flyout window.
+    /// </summary>
+    public interface ISettingsFlyoutViewModel
+    {
+        /// <summary>
+        ///     Gets or sets a value indicating whether the flyout is visible or not.
+        /// </summary>
+        bool IsOpen { get; set; }
+    }
+}
---
+++ solution/src/labelCreator/ViewModels/SettingsFlyoutViewModel.cs
@@ -0,0 +1,30 @@
+﻿namespace LabelCreator.ViewModels
+{
+    using Caliburn.Micro;
+    using LabelCreator.ViewModels.Contracts;
+
+    /// <summary>
+    ///     The view model representing the application flyout window.
+    /// </summary>
+    public class SettingsFlyoutViewModel : PropertyChangedBase, ISettingsFlyoutViewModel
+    {
+        private bool isOpen;
+
+        /// <summary>
+        ///     Gets or sets a value indicating whether the flyout is visible or not.
+        /// </summary>
+        public bool IsOpen
+        {
+            get
+            {
+                return this.isOpen;
+            }
+
+            set
+            {
+                this.isOpen = value;
+                this.NotifyOfPropertyChange("IsOpen");
+            }
+        }
+    }
+}
