f720d01eb2ee61de2370ae7e93de7fcb6fb013df
laurianesavary <lauriane.savary@gmail.com>
2016-02-09 16:03:34 +0100

    First commit FilePicker control
    Should be improved

---
+++ solution/src/labelCreator/Controls/FilePicker.xaml.cs
@@ -0,0 +1,31 @@
+﻿namespace LabelCreator.Controls
+{
+    using Microsoft.Win32;
+    using System.Windows;
+    using System.Windows.Controls;
+
+    /// <summary>
+    /// Interaction logic for FilePicker.xaml
+    /// </summary>
+    public partial class FilePicker : UserControl
+    {
+        public FilePicker()
+        {
+            this.InitializeComponent();
+        }
+
+        private void BrowseClick(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog();
+            dialog.DefaultExt = ".png";
+            dialog.Filter = "Image files (*.png, *.jpg)|*.png;*.jpg|All files (*.*)|*.*";
+
+            var result = dialog.ShowDialog();
+
+            if (result == true)
+            {
+                this.FilePickerText.Text = dialog.FileName;
+            }
+        }
+    }
+}
